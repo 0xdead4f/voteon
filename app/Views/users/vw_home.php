2662d0cd56f5df3d795df9aa2ce3cf8f2501dfc0
adivafiqri <adivafiqri@gmail.com>
2022-01-28 14:06:03 +0700

    membuat tampilan login dan home sederhana

---
+++ app/Views/users/vw_home.php
@@ -0,0 +1,14 @@
+<?= $this->extend('layout/template'); ?>
+
+
+<?= $this->section('content'); ?>
+
+<div class="contanier">
+    <div class="row">
+        <div class="col">
+            <h1>Hello World ! </h1>
+        </div>
+    </div>
+</div>
+
+<?= $this->endSection(); ?>
---
+++ app/Views/users/vw_login.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.79.0">
+    <title>Signin Template · Bootstrap v5.0</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://getbootstrap.com/docs/5.0/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/login.css">
+</head>
+
+<body>
+
+    <div class="login-box">
+        <h2>Login</h2>
+        <?php if (!empty(session()->getFlashdata('error'))) : ?>
+            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                <?php echo session()->getFlashdata('error'); ?>
+            </div>
+        <?php endif; ?>
+        <form method="post" action="<?= base_url(); ?>/login/process">
+            <?= csrf_field(); ?>
+            <div class="user-box">
+                <input type="text" name="nik" required="">
+                <label>Username</label>
+            </div>
+            <div class="user-box">
+                <input type="password" name="password" required="">
+                <label>Password</label>
+            </div>
+            <a href="#">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                Submit
+            </a>
+        </form>
+    </div>
+
+
+
+</body>
+
+</html>
