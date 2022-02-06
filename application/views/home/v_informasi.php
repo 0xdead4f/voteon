d3c644ffbfd6875baf9fa4d32a215a531b1ce2c3
adivafiqri <adivafiqri@gmail.com>
2022-02-06 12:22:53 +0700

    konfigurasi pergantian nama

--- application/views/home/v_informasi.php
+++
@@ -1,1 +0,0 @@
-<h1>Ini Informasi</h1>
--- application/views/layout/v_navbar.php
+++ application/views/layout/v_navbar.php
@@ -1,18 +1,18 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="<?= base_url(); ?>">Navbar</a>
+<nav class="navbar navbar-expand-lg navbar-light bg-light my-2">
+    <div class="container-fluid" style="padding: 0 400px;">
+        <a class="navbar-brand" href="<?= base_url(); ?>"><img src="<?= base_url(); ?>assets/home-css/img/logo.png" width="50px" alt=""></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item" id="link-li">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item mx-2" id="link-li">
                     <a class="nav-link" aria-current="page" href="<?= base_url(); ?>">Home</a>
                 </li>
-                <li class="nav-item" id="link-li">
-                    <a class="nav-link" href="<?= base_url(); ?>home/informasi">Informasi</a>
+                <li class="nav-item mx-2" id="link-li">
+                    <a class="nav-link" href="<?= base_url(); ?>#about">About</a>
                 </li>
-                <li class="nav-item" id="link-li">
+                <li class="nav-item mx-2" id="link-li">
                     <a class="nav-link" href="<?= base_url(); ?>pilih">E-Voting</a>
                 </li>
 
